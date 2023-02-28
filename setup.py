4c2a05c04ea5991e740551f65c9e2f4e2b700082
John Schreck <jsschreck@gmail.com>
2023-02-28 15:32:12 -0700

    Initial commit of env and setup scripts

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="s2sml",
+      version="0.1",
+      description="Machine learning for s2s prediction",
+      author="Maria Molina",
+      author_email="molina@ucar.edu",
+      license="MIT",
+      url="https://github.com/mariajmolina/s2sml",
+      packages=["s2sml"])
